254bc3ed634f435663ac8fedc6a0bdafd3d1e6b1
Aritra Dey <aritradey97@gmail.com>
2018-04-02 14:06:03 +0530

    Add files via upload

---
+++ CameraStable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour {
+    public GameObject Car;
+    private float carX, carY, carZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        carX = Car.transform.eulerAngles.x;
+        carY = Car.transform.eulerAngles.y;
+        carZ = Car.transform.eulerAngles.z;
+        transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+    }
+}
